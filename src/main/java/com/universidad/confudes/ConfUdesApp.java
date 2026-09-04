87def1aa825dd9e4f85d4047d1c91f2e7313a3b9
sanYesid <joseyesidquintero11@gmail.com>
2026-09-04 13:10:19 -0500

    feat: inicializar proyecto Spring Boot confudes-patrones-estructurales

---
+++ src/main/java/com/universidad/confudes/ConfUdesApp.java
@@ -0,0 +1,11 @@
+package com.universidad.confudes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfUdesApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfUdesApp.class, args);
+    }
+}
